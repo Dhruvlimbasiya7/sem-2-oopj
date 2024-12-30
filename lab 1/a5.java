ebcf620ebfcb88d232d4958e516a835c52808063
Dhruvlimbasiya7 <24031701027@darshan.ac.in>
2024-12-30 21:56:29 +0530

    Add files via upload

---
+++ lab 1/a5.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class a5
+{
+	public static void main (String[]args)
+	{
+		Scanner dl= new Scanner(System.in);
+
+		String s1= dl.nextLine();
+		String s2= dl.next();
+
+		System.out.println(s1);
+		System.out.println(s2);
+	}
+}
